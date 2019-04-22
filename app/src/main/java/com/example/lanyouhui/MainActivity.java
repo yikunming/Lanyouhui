bc4c3f9902a3aae11d7e59e3d77e091f1a3cac1d
cap <2273566160@qq.com>
2019-04-22 11:14:46 +0800

    eees

--- app/src/main/java/com/example/lanyouhui/MainActivity.java
+++ app/src/main/java/com/example/lanyouhui/MainActivity.java
@@ -14,6 +14,9 @@ import java.util.List;
 
 import Adapter.MyStateAdapter;
 
+/**
+ * 主页面
+ */
 public class MainActivity extends AppCompatActivity implements
         BottomNavigationBar.OnTabSelectedListener,ViewPager.OnPageChangeListener {
 
